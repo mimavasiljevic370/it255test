d13a4d51161a3095c61e177f890dfb76ee559142
mima <mima.vasiljevic.370@metropolitan.ac.rs>
2019-07-07 19:30:11 +0200

    Initial commit 7.2019

--- index.php
+++ index.php
@@ -2,3 +2,18 @@
         echo "Hello World";
     ?>
 
+    <head> 
+    <style type="text/css"> 
+    .centar 
+    { 
+    text-align:center; 
+    } 
+    #sadrzaj { 
+    background-color: #F5DEB3; 
+    border:1px solid transparent; 
+    -moz-border-radius: 10px; 
+    border-radius:10px; 
+    } 
+    </style> 
+    </head>
+
